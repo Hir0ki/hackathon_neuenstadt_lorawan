8645fafd05c1b4a2be81dd8f100140cbd3d0893f
Jan Denis <jandenis1402@gmail.com>
2019-10-19 13:44:25 +0200

    Coreccted config.ini

--- TTN.py
+++ TTN.py
@@ -10,3 +10,15 @@ def ttn_handler(msg,client):
     print(msg)
 
 handler = ttn.HandlerClient(config["TTN"]["appid"],config["TTN"]["apikey"])
+
+mqtt_client = handler.data()
+mqtt_client.set_uplink_callback(uplink_callback)
+mqtt_client.connect()
+time.sleep(60)
+mqtt_client.close()
+
+app_client =  handler.application()
+my_app = app_client.get()
+print(my_app)
+my_devices = app_client.devices()
+print(my_devices)
